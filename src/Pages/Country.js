eb50f22457dd98692356f35cdcef3dbd549074f8
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-12-07 16:18:14 -0500

    Add search bar and optimize grid

--- src/Pages/Country.js
+++ src/Pages/Country.js
@@ -2,6 +2,8 @@ import { NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import React, { useEffect } from 'react';
 import { getEvents } from '../Redux/EventsSlice'
+import { ImSearch } from 'react-icons/im'
+import '../Styles/Country.css'
 
 const Country = () => {
 
@@ -18,11 +20,14 @@ const Country = () => {
 
     return (
       <>
-        <h1>The country page is working</h1>
+        <form id='searchBar'>
+          <input type='text' id='searchInput' />
+          <button type='submit' id='searchBtn'><ImSearch style={{color: 'red', fontSize: '1rem'}}/></button>
+        </form>
         <div id='eventsGrid'>
         {events.map((event) => (
           <div className='eventCard'>
-            <img src={event.cardImage} alt='event' style={{width: '300px'}}/>
+            <img src={event.cardImage} alt='event' style={{width: '300px', height: '200px'}}/>
             <h3 key={event.id}>{event.name}</h3>
           </div>
           ))}
