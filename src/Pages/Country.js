c8708c8e03d1db70c932213bb02a477e835e6b90
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-12-09 16:59:11 -0500

    Style hover cards

--- src/Pages/Country.js
+++ src/Pages/Country.js
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import React, { useEffect, useState } from 'react';
-import { getEvents, searchByName } from '../Redux/EventsSlice'
+import { getEvents } from '../Redux/EventsSlice'
 import { ImSearch } from 'react-icons/im'
 import { RxDotFilled } from 'react-icons/rx'
 import '../Styles/Country.css'
